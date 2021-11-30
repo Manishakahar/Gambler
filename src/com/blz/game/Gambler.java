942e4eb2b7d9087e3a98cc122854dc5fb624d8a5
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-30 09:12:23 +0530

    Refactor: added consant value

---
+++ src/com/blz/game/Gambler.java
@@ -0,0 +1,14 @@
+package com.blz.game;
+public class Gambler {
+    public static void gambling(){
+        //Initialize consant
+        int STAKE_PER_DAY=100;
+        int BET = 1;
+        System.out.println(STAKE_PER_DAY);
+        System.out.println(BET);
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to gambler simulation");
+        gambling();
+    }
+}
