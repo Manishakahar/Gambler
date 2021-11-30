2b5f699d015993eb7e92ca12efea065b151d5f02
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-30 08:50:59 +0530

    Added: checking initialize consant value

--- src/com/blz/uc/Gambler.java
+++ src/com/blz/uc/Gambler.java
@@ -1,7 +1,10 @@
 package com.blz.uc;
-
 public class Gambler {
     public static void main(String[] args) {
         System.out.println("Welcome to gambler simulation");
+
+        //Initialize consant
+         int STAKE_PER_DAY=100;
+         int BET = 1;
     }
 }
